f4dba506a2650d46c5c2c947ebf041d20f213c34
Eugene Chernyshov <eugene@thefloorcyber.com>
2022-12-10 08:17:28 +0200

    day 10 of 2022

---
+++ 2022/10/main.py
@@ -0,0 +1,49 @@
+with open('data.txt', 'r') as f:
+    data = f.read()
+
+
+def gen_stream():
+    time = 0
+    value = 1
+    for line in data.strip().splitlines():
+        if line.startswith('noop'):
+            time += 1
+            yield value
+        elif line.startswith('addx'):
+            yield value
+            yield value
+            _, v = line.split()
+            value += int(v)
+            time += 2
+
+
+def task1():
+    cycles = [
+        20, 60, 100, 140, 180, 220
+    ]
+
+    res = 0
+    prev = 1
+
+    stream = gen_stream()
+    start = 0
+    for end in cycles:
+        for _ in range(start, end):
+            value = next(stream, prev)
+            prev = value
+        res += prev * end
+        start = end
+    return res
+
+
+def task2():
+    s = gen_stream()
+    for y in range(6):
+        for x in range(40):
+            value = next(s)
+            print('.#'[value - 1 <= x <= value + 1], end='')
+        print()
+
+
+print(task1())
+task2()
