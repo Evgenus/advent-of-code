ba271edbcddb1c5fb347cc1968d29d1d7f5a5c09
Eugene Chernyshov <eugene@thefloorcyber.com>
2022-12-12 10:19:43 +0200

    day 6 of 2022

---
+++ 2022/06/main.py
@@ -0,0 +1,26 @@
+from utils import str_iter_chunks
+
+
+def read_data(filename):
+    with open(filename, 'r') as stream:
+        data = stream.read()
+
+    return data.strip().splitlines()
+
+
+def task(filename, size):
+    lines = read_data(filename)
+
+    res = []
+    for line in lines:
+        for i, chunk in enumerate(str_iter_chunks(line, size)):
+            if len(set(chunk)) == size:
+                res.append(i + size)
+                break
+    return res
+
+
+assert task('test.txt', 4) == [7, 5, 6, 10, 11]
+assert task('test.txt', 14) == [19, 23, 23, 29, 26]
+assert task('data.txt', 4) == [1582]
+assert task('data.txt', 14) == [3588]
