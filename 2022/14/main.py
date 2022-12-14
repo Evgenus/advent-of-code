c6853a43ae569c2e9c5cfe2b0f8543fa53fce1c7
Eugene Chernyshov <eugene@thefloorcyber.com>
2022-12-14 11:16:01 +0200

    generalize

--- 2022/14/main.py
+++ 2022/14/main.py
@@ -1,14 +1,6 @@
 from utils import *
 
 
-def draw(matrix, x1, y1, x2, y2):
-    while x1 != x2 or y1 != y2:
-        matrix[x1][y1] = 1
-        x1, y1 = cell_move_towards((x1, y1), (x2, y2))
-
-    matrix[x1][y1] = 1
-
-
 def read_data(filename):
     with open(filename, 'r') as f:
         data = f.read()
@@ -25,7 +17,7 @@ def read_data(filename):
             x1, y1 = pairs[i - 2], pairs[i - 1]
             x2, y2 = pairs[i], pairs[i + 1]
 
-            draw(matrix, y1, x1, y2, x2)
+            matrix_drawline(matrix, (y1, x1), (y2, x2), 1)
             maxy = max(maxy, y1, y2)
 
     return matrix, maxy
@@ -33,7 +25,7 @@ def read_data(filename):
 
 def print_result(matrix, visited):
     for x, y in visited:
-        matrix[y][x] = '2'
+        matrix[y][x] = 2
 
     boundaries = matrix_boundaries(matrix, lambda cell: cell != 0)
     matrix_print(matrix_crop(matrix, *boundaries), translation={'0': '.', '1': '#', '2': 'o'})
