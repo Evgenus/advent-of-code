1d3c6cce53b01cf1d6067ae5f8c7f4e9840c1b79
Eugene Chernyshov <eugene@thefloorcyber.com>
2022-12-14 11:16:17 +0200

    refactor windows/chunks

--- 2022/06/main.py
+++ 2022/06/main.py
@@ -1,4 +1,4 @@
-from utils import str_iter_chunks
+from utils import *
 
 
 def read_data(filename):
@@ -13,7 +13,7 @@ def task(filename, size):
 
     res = []
     for line in lines:
-        for i, chunk in enumerate(str_iter_chunks(line, size)):
+        for i, chunk in enumerate(iter_window(line, size)):
             if len(set(chunk)) == size:
                 res.append(i + size)
                 break
