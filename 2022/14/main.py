18e944c30508f51f416d1a932adc588d453c8224
Eugene Chernyshov <eugene@thefloorcyber.com>
2022-12-14 09:09:43 +0200

    refactored

--- 2022/14/main.py
+++ 2022/14/main.py
@@ -1,94 +1,92 @@
 from utils import *
 
 
+def draw(matrix, x1, y1, x2, y2):
+    while x1 != x2 or y1 != y2:
+        matrix[x1][y1] = 1
+        x1, y1 = cell_move_towards((x1, y1), (x2, y2))
+
+    matrix[x1][y1] = 1
+
+
 def read_data(filename):
     with open(filename, 'r') as f:
         data = f.read()
 
-    return data.strip().splitlines()
+    lines = data.strip().splitlines()
+    matrix = [
+        [0] * 1000 for _ in range(1000)
+    ]
+
+    maxy = 0
+    for line in lines:
+        pairs = str_integers(line)
+        for i in range(2, len(pairs), 2):
+            x1, y1 = pairs[i - 2], pairs[i - 1]
+            x2, y2 = pairs[i], pairs[i + 1]
+
+            draw(matrix, y1, x1, y2, x2)
+            maxy = max(maxy, y1, y2)
+
+    return matrix, maxy
+
+
+def print_result(matrix, visited):
+    for x, y in visited:
+        matrix[y][x] = '2'
+
+    boundaries = matrix_boundaries(matrix, lambda cell: cell != 0)
+    matrix_print(matrix_crop(matrix, *boundaries), translation={'0': '.', '1': '#', '2': 'o'})
+
+
+def fall_one_step(matrix, visited, x, y):
+    for nx in (x, x - 1, x + 1):
+        if matrix[y + 1][nx] != 1 and (nx, y + 1) not in visited:
+            return nx, y + 1
+    return x, y
 
 
 def task1(filename):
-    matrix = [
-        [0] * 1000 for _ in range(1000)
-    ]
+    matrix, maxy = read_data(filename)
 
-    def draw(x1, y1, x2, y2):
-        while x1 != x2 or y1 != y2:
-            matrix[x1][y1] = 1
-            x1, y1 = cell_move_towards((x1, y1), (x2, y2))
-        matrix[x1][y1] = 1
-
-    maxy = 0
-    lines = read_data(filename)
-    for line in lines:
-        numbers = str_integers(line)
-        for i in range(2, len(numbers), 2):
-            x1, y1 = numbers[i - 2], numbers[i - 1]
-            x2, y2 = numbers[i], numbers[i + 1]
-
-            draw(x1, y1, x2, y2)
-            maxy = max(maxy, y1, y2)
-
-    # print(maxy)
+    start = 500, 0
     visited = set()
-
     res = 0
     while True:
-        x, y = 500, 0
+        x, y = start
         while True:
-            ny = y + 1
-            for nx in (x, x - 1, x + 1):
-                if matrix[nx][ny] != 1 and (nx, ny) not in visited:
-                    x, y = nx, ny
-                    break
-            else:
-                res += 1
-                visited.add((x, y))
+            nx, ny = fall_one_step(matrix, visited, x, y)
+            if (nx, ny) == (x, y):
                 break
+            x, y = nx, ny
             if y > maxy:
+                # print_result(matrix, visited)
                 return res
+        res += 1
+        visited.add((x, y))
 
 
 def task2(filename):
-    matrix = [
-        [0] * 1000 for _ in range(1000)
-    ]
+    matrix, maxy = read_data(filename)
 
-    def draw(x1, y1, x2, y2):
-        while x1 != x2 or y1 != y2:
-            matrix[x1][y1] = 1
-            x1, y1 = cell_move_towards((x1, y1), (x2, y2))
-        matrix[x1][y1] = 1
-
-    maxy = 0
-    lines = read_data(filename)
-    for line in lines:
-        numbers = str_integers(line)
-        for i in range(2, len(numbers), 2):
-            x1, y1 = numbers[i - 2], numbers[i - 1]
-            x2, y2 = numbers[i], numbers[i + 1]
-            maxy = max(maxy, y1, y2)
-            draw(x1, y1, x2, y2)
-
+    start = 500, 0
     visited = set()
     res = 0
-    while (500, 0) not in visited:
-        x, y = 500, 0
-        while (500, 0) not in visited:
-            if y == maxy + 2:
-                visited.add((x, y))
+    while True:
+        x, y = start
+        while True:
+            nx, ny = fall_one_step(matrix, visited, x, y)
+            if (nx, ny) == (x, y):
                 break
+            x, y = nx, ny
+            if y > maxy:
+                break
+        res += 1
+        visited.add((x, y))
+        if (x, y) == start:
+            break
 
-            ny = y + 1
-            for nx in (x, x - 1, x + 1):
-                if matrix[nx][ny] != 1 and (nx, ny) not in visited:
-                    x, y = nx, ny
-                    break
-            else:
-                res += 1
-                visited.add((x, y))
-                break
+    # print_result(matrix, visited)
     return res
 
 
