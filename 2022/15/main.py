974d6d5f637314a5b58d9bbaa8ec593f7eccda2a
Eugene Chernyshov <eugene@thefloorcyber.com>
2022-12-15 08:19:38 +0200

    refactor

--- 2022/15/main.py
+++ 2022/15/main.py
@@ -6,77 +6,62 @@ def read_data(filename):
         data = f.read()
 
     lines = data.strip().splitlines()
-    return lines
+    sensors = {}
+    beacons = set()
+    for line in lines:
+        sx, sy, bx, by = str_integers(line)
+        dist = cell_dist4((sx, sy), (bx, by))
+        sensors[sx, sy] = dist
+        beacons.add((bx, by))
+
+    return sensors, beacons
+
+
+def check(distances, x, y):
+    for sx, sy in distances:
+        if cell_dist4((sx, sy), (x, y)) <= distances[sx, sy]:
+            return False
+    return True
 
 
 def task1(filename, row):
-    lines = read_data(filename)
-    distances = {}
+    sensors, beacons = read_data(filename)
+
     minx = float('inf')
     maxx = -float('inf')
-    beacons = set()
-    for line in lines:
-        sx, sy, bx, by = str_integers(line)
-        dist = cell_dist4((sx, sy), (bx, by))
-        distances[sx, sy] = dist
-        minx = min(minx, bx)
-        maxx = max(maxx, bx)
-        beacons.add((bx, by))
+
+    for a, d in sensors.items():
+        r = (d - abs(a[1] - row))
+        minx = min(minx, a[0] - r)
+        maxx = max(maxx, a[0] + r)
 
     beacons = {x for x, y in beacons if y == row}
-    ranges = set()
-    for sx, sy in distances:
-        r = (distances[sx, sy] - abs(sy - row))
-        if r >= 0:
-            s = set(range(sx - r, sx + r + 1))
-            ranges |= s
-
-    ranges = ranges - beacons
-
-    return len(ranges)
-    # for col in range(10):
-
-
-#load_input()
-# print(task1('test.txt', 10))
-# print(task1('data.txt', 2000000))
+    res = 0
+    for x in range(minx, maxx + 1):
+        if x in beacons:
+            continue
+        res += not check(sensors, x, row)
+    return res
 
 
 def task2(filename, cap):
-    lines = read_data(filename)
-    distances = {}
-    minx = float('inf')
-    maxx = -float('inf')
-    beacons = set()
-    for line in lines:
-        sx, sy, bx, by = str_integers(line)
-        dist = cell_dist4((sx, sy), (bx, by))
-        distances[sx, sy] = dist
-        minx = min(minx, bx)
-        maxx = max(maxx, bx)
-        beacons.add((bx, by))
+    sensors, _ = read_data(filename)
 
-    def check(x, y):
-        for sx, sy in distances:
-            if cell_dist4((sx, sy), (x, y)) <= distances[sx, sy]:
-                return False
-        return True
-
-    sensors = list(distances.keys())
-    for a in sensors:
-        print('.')
-        d = distances[a] + 1
+    for a, d in sensors.items():
+        d += 1
         for y in range(a[1] - d, a[1] + d):
             if y > cap or y < 0:
                 break
             r = (d - abs(a[1] - y))
             x1 = a[0] - r
             x2 = a[0] + r
-            if 0 <= x1 <= cap and check(x1, y):
+            if 0 <= x1 <= cap and check(sensors, x1, y):
                 return x1 * 4000000 + y
-            if 0 <= x2 <= cap and check(x2, y):
+            if 0 <= x2 <= cap and check(sensors, x2, y):
                 return x2 * 4000000 + y
 
 
-#print(task2('test.txt'))
-print(task2('data.txt', 4000000))
+assert task1('test.txt', 10) == 26
+assert task1('data.txt', 2000000) == 4793062
+assert task2('test.txt', 20) == 56000011
+assert task2('data.txt', 4000000) == 10826395253551
